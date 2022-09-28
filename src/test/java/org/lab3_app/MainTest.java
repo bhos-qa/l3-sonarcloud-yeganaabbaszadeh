c8ea90cf4da01df25c7e941bbe04b3af8eeadd0f
Yegana Abbaszadeh <yegana.abbaszadeh@gmail.com>
2022-09-28 15:11:53 +0400

    Added Test

--- src/test/java/org/lab3_app/MainTest.java
+++ src/test/java/org/lab3_app/MainTest.java
@@ -18,5 +18,9 @@ public class MainTest {
     void managePermissionTest() {
         permissionManager.set(Main.PermissionLevel.USER);
         assertEquals("USER", permissionManager.get());
+        permissionManager.set(Main.PermissionLevel.DEVELOPER);
+        assertEquals("DEVELOPER", permissionManager.get());
+        permissionManager.set(Main.PermissionLevel.ADMIN);
+        assertEquals("ADMIN", permissionManager.get());
     }
 }
