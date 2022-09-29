57b2835fc82a3d6ab86d994421f2cc28a5a34b9d
Yegana Abbaszadeh <yegana.abbaszadeh@gmail.com>
2022-09-29 08:53:08 +0400

    Update Test

--- src/test/java/org/lab3_app/PermissionManagerTest.java
+++ src/test/java/org/lab3_app/PermissionManagerTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class PermissionManagerTest {
+class PermissionManagerTest {
 
     PermissionManager permissionManager;
 
